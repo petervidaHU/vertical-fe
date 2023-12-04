5f2ba76050d77089dd236d4fb757144406de3704
peter_vida <peter_vida1@epam.com>
2023-12-04 16:36:53 +0100

    implement scrolling event handler

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export const App = () => <h1>Hello from React!</h1>;
+export const App = () => {
+  const [scrollAmount, setScrollAmount] = useState(0);
+  
+  const handleScroll = (event: WheelEvent) => {
+    event.preventDefault();
+    const deltaY = event.deltaY < 0 ? Math.abs(event.deltaY) : event.deltaY * -1;
+
+    setScrollAmount((prev) => prev + deltaY > 0 ? prev + deltaY : 0);
+  };
+
+  useEffect(() => {
+    document.addEventListener('wheel', handleScroll, { passive: false });
+
+    return () => {
+      document.removeEventListener('wheel', handleScroll);
+    };
+  }, []);
+
+  console.log('scroll: ', scrollAmount);
+  
+  return (
+    <h1>Hello from React!</h1>
+  )
+
+};
