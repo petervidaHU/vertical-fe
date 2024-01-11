359f5f2f74ccb8cf23c6e5af13da313f203b9ef7
peter_vida <peter_vida1@epam.com>
2024-01-11 22:00:55 +0100

    prod backend url

--- src/API/storyAPI.ts
+++ src/API/storyAPI.ts
@@ -20,7 +20,7 @@ export interface StoriesResponse {
 };
 
 // const baseUrl = process.env.REACT_APP_BACKEND_API;
-const baseUrl = 'https://vertical-gfvuggaeo-petervidahu.vercel.app/';
+const baseUrl = 'https://vertical-be.vercel.app/';
 
 export const storiesApi = createApi({
   reducerPath: 'storiesApi',
